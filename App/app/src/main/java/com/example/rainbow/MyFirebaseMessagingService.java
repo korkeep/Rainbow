bc25b8542f0fb23915f884687f1a37c2e8f1e93e
KSS <korkeep@naver.com>
2020-12-18 20:37:13 +0900

    Rainbow Application Added

---
+++ App/app/src/main/java/com/example/rainbow/MyFirebaseMessagingService.java
@@ -0,0 +1,107 @@
+package com.example.rainbow;
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMsgService";
+
+//Token Log 정의
+@Override
+public void onNewToken(String token){
+    super.onNewToken(token);
+    Log.d("TOKEN LOG", "새로운 토큰: " + token);
+}
+
+    //FireBase 메시지 수신
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+    super.onMessageReceived(remoteMessage); String title = remoteMessage.getData().get("title");//firebase에서 보낸 메세지의 title
+        String message = remoteMessage.getData().get("message");//firebase에서 보낸 메세지의 내용
+        String test = remoteMessage.getData().get("test");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("test", test);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            String channel = "채널";
+            String channel_nm = "채널명";
+
+            NotificationManager notichannel = (android.app.NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel channelMessage = new NotificationChannel(channel, channel_nm,
+                    android.app.NotificationManager.IMPORTANCE_DEFAULT);
+            channelMessage.setDescription("채널에 대한 설명.");
+            channelMessage.enableLights(true);
+            channelMessage.enableVibration(true);
+            channelMessage.setShowBadge(false);
+            channelMessage.setVibrationPattern(new long[]{1000, 1000});
+            notichannel.createNotificationChannel(channelMessage);
+
+            //푸시알림을 Builder를 이용하여 만듭니다.
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this, channel)
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentTitle(title)//푸시알림의 제목
+                            .setContentText(message)//푸시알림의 내용
+                            .setChannelId(channel)
+                            .setAutoCancel(true)//선택시 자동으로 삭제되도록 설정.
+                            .setContentIntent(pendingIntent)//알림을 눌렀을때 실행할 인텐트 설정.
+                            .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(9999, notificationBuilder.build());
+
+        } else {
+            NotificationCompat.Builder notificationBuilder =
+                    new NotificationCompat.Builder(this, "")
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentTitle(title)
+                            .setContentText(message)
+                            .setAutoCancel(true)
+                            .setContentIntent(pendingIntent)
+                            .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(9999, notificationBuilder.build());
+
+        }
+    }
+
+
+}
